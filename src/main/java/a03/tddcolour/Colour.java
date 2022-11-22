f51ba289f001cf5903483f799ed49dfc2a077e36
Jin Yijie <120109266@umail.ucc.ie>
2022-11-22 23:08:16 +0000

    Add implementation of constructor.

--- src/main/java/a03/tddcolour/Colour.java
+++ src/main/java/a03/tddcolour/Colour.java
@@ -1,4 +1,12 @@
 package a03.tddcolour;
 
 public class Colour {
+    private float red;
+    private float green;
+    private float blue;
+    public Colour(float r, float g, float b) {
+        red = r;
+        green = g;
+        blue = b;
+    }
 }
