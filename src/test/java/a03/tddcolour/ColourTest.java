db411a6d91e235d9056cd0e93fdaf20af21a5f8a
Jin Yijie <120109266@umail.ucc.ie>
2022-11-22 23:07:50 +0000

    Add test of constructor-takes-three-floats.

--- src/test/java/a03/tddcolour/ColourTest.java
+++ src/test/java/a03/tddcolour/ColourTest.java
@@ -1,4 +1,15 @@
 package a03.tddcolour;
 
-public class ColourTest {
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+class ColourTest {
+    @Test
+    @DisplayName("A constructor should take any combination of three parameters of float values between 0.0 to 1.0.")
+    void constructorTakeThreeFloat() {
+        Colour c0 = new Colour(0F, 0F, 0F);
+        Colour c1 = new Colour(1F, 1F, 1F);
+    }
 }
+
+
