1d233838f2dcee2fb930f7565166a5aa92d59db0
Jin Yijie <120109266@umail.ucc.ie>
2022-11-22 23:32:04 +0000

    Add test of constructor parameters out of range.

--- src/test/java/a03/tddcolour/ColourTest.java
+++ src/test/java/a03/tddcolour/ColourTest.java
@@ -2,6 +2,7 @@ package a03.tddcolour;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ColourTest {
     @Test
@@ -10,6 +11,13 @@ class ColourTest {
         Colour c0 = new Colour(0F, 0F, 0F);
         Colour c1 = new Colour(1F, 1F, 1F);
     }
+
+    @Test
+    @DisplayName("A constructor should throw IllegalArgumentException if any of the parameter value is not between 0.0 and 1.0")
+    void constructorThrowOutOfRange() {
+        Exception exception =  assertThrows(IllegalArgumentException.class, () -> new Colour(2F, 0F, 0F));
+    }
+
 }
 
 
