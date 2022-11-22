7a9e1a283611e1266c186cd94b76d2a39436e943
Jin Yijie <120109266@umail.ucc.ie>
2022-11-22 23:32:29 +0000

    Modified constructor.

--- src/main/java/a03/tddcolour/Colour.java
+++ src/main/java/a03/tddcolour/Colour.java
@@ -5,6 +5,9 @@ public class Colour {
     private float green;
     private float blue;
     public Colour(float r, float g, float b) {
+        if (r > 1F || r < 0F|| g > 1F || g < 0F || b > 1F || b < 0F) {
+            throw new IllegalArgumentException("Parameter out of range.");
+        }
         red = r;
         green = g;
         blue = b;
