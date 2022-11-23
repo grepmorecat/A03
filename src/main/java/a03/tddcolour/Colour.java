7cfe67c9ccab143de90954fe12bb1a977430763c
Jin Yijie <120109266@umail.ucc.ie>
2022-11-23 16:00:20 +0000

    Add overload constructor that takes one parameter.

--- src/main/java/a03/tddcolour/Colour.java
+++ src/main/java/a03/tddcolour/Colour.java
@@ -4,6 +4,8 @@ public class Colour {
     private float red;
     private float green;
     private float blue;
+    private int rgbValue;
+
     public Colour(float r, float g, float b) {
         if (r > 1F || r < 0F|| g > 1F || g < 0F || b > 1F || b < 0F) {
             throw new IllegalArgumentException("Parameter out of range.");
@@ -11,5 +13,12 @@ public class Colour {
         red = r;
         green = g;
         blue = b;
+    }
+    public Colour(int rgb) {
+        if (rgb > 0xFFFFFF) {
+            throw new IllegalArgumentException("Parameter out of range.");
+        }
+        rgbValue = rgb;
+
     }
 }
