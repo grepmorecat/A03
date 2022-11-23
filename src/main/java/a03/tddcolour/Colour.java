76de2dd6ad6ff03f89ecfd3c1888d5888bb977d5
Jin Yijie <120109266@umail.ucc.ie>
2022-11-23 16:39:22 +0000

    Fix logics.

--- src/main/java/a03/tddcolour/Colour.java
+++ src/main/java/a03/tddcolour/Colour.java
@@ -1,9 +1,9 @@
 package a03.tddcolour;
 
 public class Colour {
-    private float red;
-    private float green;
-    private float blue;
+    float red;
+    float green;
+    float blue;
 
     public Colour(float r, float g, float b) {
         if (r > 1F || r < 0F|| g > 1F || g < 0F || b > 1F || b < 0F) {
@@ -17,9 +17,8 @@ public class Colour {
         if (rgb > 0xFFFFFF || rgb < 0) {
             throw new IllegalArgumentException("Parameter out of range.");
         }
-        int bitmask = 0xFF;
-        red = (float) ((double)((rgb & (bitmask * 16)) / 16) / (double)0xFF);
-        green = (float) ((double)((rgb & (bitmask * 4)) / 4) / (double)0xFF);
-        blue = (float) ((double)(rgb & bitmask) / (double)0xFF);
+        red = (float) (((rgb >> 16) & 0xFF) / 256.0);
+        green = (float) (((rgb >> 8) & 0xFF) / 256.0);
+        blue = (float) ((rgb & 0xFF) / 256.0);
     }
 }
